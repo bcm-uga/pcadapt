0012a9ef5dd30d2fbf4bfc9fc15734d466c3fe47
keurcien <keurcien.luu@gmail.com>
2016-10-18 15:50:54 +0200

    Change pool procedure

--- R/pcadaptPool.R
+++ R/pcadaptPool.R
@@ -44,6 +44,7 @@ corpca = function(data,K){
 #' @param K an integer specifying the number of principal components to retain.
 #' @param min.maf a value between \code{0} and \code{0.45} specifying the threshold
 #' of minor allele frequencies above which p-values are computed.
+#' @param cover.matrix a matrix specifying the average coverage per genetic marker and per population.
 #'
 #' @importFrom robust covRob
 #' @importFrom MASS cov.rob
@@ -53,24 +54,42 @@ corpca = function(data,K){
 #' 
 #' @export
 #'
-create.pcadapt.pool = function(data,K,min.maf){
+create.pcadapt.pool = function(data,K,min.maf,cover.matrix=NULL){
   nSNP <- ncol(data)
   nPOP <- nrow(data)
+  # New procedure
+  z.matrix <- array(0,dim=c(nPOP,nSNP))
+  for (k in 1:nSNP){
+    for (n in 1:nPOP){
+      n_i <- cover.matrix[n,k]
+      f_i <- data[n,k]
+      se <- sqrt(abs(f_i*(1-f_i))/n_i)
+      if ((!is.na(se)) && (se > 0)){
+        z.matrix[n,k] <- f_i/se 
+      } else {
+        z.matrix[n,k] <- f_i/0.01
+      }
+    }
+  }
+  # End new procedure
   res <- corpca(data=data,K=K)
   freq <- apply(data,2,FUN=function(x){mean(x,na.rm=TRUE)})
   res$maf <- as.vector(pmin(freq,1-freq))
   res$loadings[res$maf<min.maf] <- NA 
+  z.matrix[,res$maf<min.maf] <- NA
   res$stat <- array(NA,dim=nSNP)
-  finite.list <- which(!is.na(apply(abs(res$loadings),1,sum)))
-  if (K>1){
-    res$stat[finite.list] <- as.vector(robust::covRob(res$loadings,na.action=na.omit,estim="pairwiseGK")$dist)
-    res$gif <- median(res$stat,na.rm=TRUE)/qchisq(0.5,df=K)
-  } else {
-    onedcov <- as.vector(MASS::cov.rob(res$loadings[finite.list,1]))
-    res$gif <- onedcov$cov[1]
-    res$stat <- (res$zscores[,1]-onedcov$center)^2
-  }
-  res$chi2.stat <- res$stat/res$gif
+  finite.list <- which(!is.na(apply(abs(z.matrix),2,sum)))
+#   if (K>1){
+#     res$stat[finite.list] <- as.vector(robust::covRob(res$loadings,na.action=na.omit,estim="pairwiseGK")$dist)
+#     res$gif <- median(res$stat,na.rm=TRUE)/qchisq(0.5,df=K)
+#   } else {
+#     onedcov <- as.vector(MASS::cov.rob(res$loadings[finite.list,1]))
+#     res$gif <- onedcov$cov[1]
+#     res$stat <- (res$zscores[,1]-onedcov$center)^2
+#   }
+#   res$chi2.stat <- res$stat/res$gif
+  res$stat <- as.vector(robust::covRob(t(z.matrix),na.action=na.omit,estim = "pairwiseGK")$dist)
+  res$chi2.stat <- res$stat
   # Compute p-values
   res$pvalues <- compute.pval(res$chi2.stat,K,method="mahalanobis")
   class(res) <- 'pcadapt'
