2240545a1b149253bdbd52ddb34ed28bb5259f4a
keurcien <keurcien.luu@gmail.com>
2017-05-23 01:07:27 +0200

    Click to plot

--- inst/shiny-examples/app-pcadapt/ui/tab-pca.R
+++ inst/shiny-examples/app-pcadapt/ui/tab-pca.R
@@ -1,7 +1,7 @@
 tabPanel(title = strong("PCA"),
          fluidRow(
-           column(6, numericInput("i", label = "i", value = 1, min = 1)),
-           column(6, numericInput("j", label = "j", value = 2, min = 1))
+           column(6, numericInput("i", label = "x-axis", value = 1, min = 1)),
+           column(6, numericInput("j", label = "y-axis", value = 2, min = 1))
          ),
          fixedRow(
            column(10, 
