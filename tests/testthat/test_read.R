f0e320fadedfdc64104544a559fd9230bcbf44de
privefl <florian.prive.21@gmail.com>
2018-02-19 10:04:12 +0100

    new bed data

--- tests/testthat/test_read.R
+++ tests/testthat/test_read.R
@@ -10,8 +10,8 @@ context("READ_PCADAPT")
 bed <- system.file("extdata", "geno3pops.bed", package = "pcadapt")
 bed2 <- read.pcadapt(bed, type = "bed")
 expect_equal(unclass(bed2), bed, check.attributes = FALSE)
-expect_equal(attr(bed2, "n"), 517)
-expect_equal(attr(bed2, "p"), 4542)
+expect_equal(attr(bed2, "n"), 150)
+expect_equal(attr(bed2, "p"), 1500)
 expect_s3_class(bed2, "pcadapt_bed")
 
 ################################################################################
